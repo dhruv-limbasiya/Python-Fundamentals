282d0893758df7e9aafb381aff90b4c83a428707
dhruv-limbasiya <limbasiyadhruv735@gmail.com>
2026-03-10 18:25:51 +0530

    caesar cipher some improvment

--- Day 8 (Caesar cipher)/step_3.py
+++ Day 8 (Caesar cipher)/step_3.py
@@ -1,11 +1,6 @@
 alphabets= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
              'v', 'w', 'x', 'y', 'z']
 
-direction = input("type 'encode' for Encrypt, type 'decode' for decrypt: ").lower()
-word = input("Enter your message: ").lower().strip()
-shift = int(input("Enter Shift amount: "))
-
-
 def caesar(original_text, shift_amount, encode_or_decode):
     cipher_word = ""
     if encode_or_decode == "decode":
@@ -20,4 +15,17 @@ def caesar(original_text, shift_amount, encode_or_decode):
     print(f"here is the {encode_or_decode}d result: {cipher_word}")
 
 
-caesar(word, shift, direction)
+
+should_continue = True
+
+while should_continue == True:
+    direction = input("type 'encode' for Encrypt, type 'decode' for decrypt: ").lower()
+    word = input("Enter your message: ").lower().strip()
+    shift = int(input("Enter Shift amount: "))
+
+    caesar(word, shift, direction)
+
+    restart = input("Type 'Yes' if you want to go again. Otherwise, type 'no': ").lower()
+    if restart == "no":
+        should_continue=False
+        print("Good Bye")
