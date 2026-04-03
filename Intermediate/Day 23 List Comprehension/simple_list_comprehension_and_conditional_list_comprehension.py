668cb71d1ae9f063c094450db87905b3ea583f67
dhruv-limbasiya <limbasiyadhruv735@gmail.com>
2026-04-03 07:38:44 +0530

    Conditional List comprehension

--- Intermediate/Day 23 List Comprehension/simple_list_comprehension_and_conditional_list_comprehension.py
+++ Intermediate/Day 23 List Comprehension/simple_list_comprehension_and_conditional_list_comprehension.py
@@ -1,3 +1,4 @@
+from http.cookiejar import uppercase_escaped_char
 
 # in normal way we can add number by 1 like that
 numbers = [1,2,3]
@@ -19,3 +20,14 @@ print(name_list)
 #create a list which includes double of range which is 1,2,3,4
 double_list = [i*2 for i in range(1,5)]
 print(double_list)
+
+
+# conditional list comprehension
+# syntax : new_list =[new_item for item in list if test]
+
+names = ["Alex", "Beth", "Carolina", "Dave", "Elenor", "Fraddie"]
+short_name = [n for n in names if len(n) < 5]
+print(short_name)
+
+upper_case = [n.upper() for n in names if len(n) > 5]
+print(upper_case)
