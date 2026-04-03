fd90207c451f68504e7efa24d25a76bcbafa86bf
dhruv-limbasiya <limbasiyadhruv735@gmail.com>
2026-04-03 09:36:30 +0530

    Dictionary comprehension

--- Intermediate/Day 23 List Comprehension/dictionary_comprehension.py
+++ Intermediate/Day 23 List Comprehension/dictionary_comprehension.py
@@ -14,6 +14,7 @@ student_scores = {name : random.randint(1, 100) for name in names}
 # in this dict. comprehension first python interpreter go in for loop and ietrate in given list and than
 # assign that name as key and it value is random number
 
+
 # create a new dictionary based on value in an existing dictionay win conditionEx:
 passed_student ={student : student_scores[student] for student in student_scores if student_scores[student] >= 60}
 # second way to create dict. comprehension
