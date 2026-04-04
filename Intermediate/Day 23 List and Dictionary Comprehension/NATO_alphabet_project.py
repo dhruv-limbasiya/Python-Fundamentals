1eddc53685f6c14a49647fd31d94f3ed5a3af461
dhruv-limbasiya <limbasiyadhruv735@gmail.com>
2026-04-04 08:46:04 +0530

    NATO project

---
+++ Intermediate/Day 23 List and Dictionary Comprehension/NATO_alphabet_project.py
@@ -0,0 +1,16 @@
+# with open("nato_phonetic_alphabet.csv") as file:
+#     print(file.read())
+
+import pandas as pd
+
+data_frame = pd.read_csv("nato_phonetic_alphabet.csv")
+# print(data_frame)
+
+data_dict = {row.letter : row.code for (index, row) in data_frame.iterrows()}
+
+print(data_dict)
+
+word = input("Enter a Word: ").upper()
+
+output = [data_dict[letter] for letter in word]
+print(output)
--- Intermediate/Day 23 List and Dictionary Comprehension/loop_in_pandas.py
+++ Intermediate/Day 23 List and Dictionary Comprehension/loop_in_pandas.py
@@ -11,12 +11,17 @@ student_dict = {
 import pandas as pd
 
 student_data_frame = pd.DataFrame(student_dict)
-print(student_data_frame)
+# print(student_data_frame)
 
 #loop through a data frame
-for (key, value) in student_data_frame.items():
-    print(value)
+# for (key, value) in student_data_frame.items():
+#     print(value)
 
 # loop through rows of data frame
 for(index, row) in student_data_frame.iterrows():
-    print(row.score)
+    print(row)
+
+# for (index,row) in student_data_frame.iterrows():
+#     if row.student == "Angela":
+#         print(row.score)
+
