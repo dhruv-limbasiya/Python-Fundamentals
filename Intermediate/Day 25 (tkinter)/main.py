363add18e15ba88cb87b6b66c286bea2824cad13
dhruv-limbasiya <limbasiyadhruv735@gmail.com>
2026-04-10 12:23:38 +0530

    label in tkinter

--- Intermediate/Day 25 (tkinter)/main.py
+++ Intermediate/Day 25 (tkinter)/main.py
@@ -8,4 +8,13 @@ window.minsize(width=500, height=300)
 my_label = tkt.Label(text="It's label", font=("Arial", 24, "bold"))
 my_label.pack(side="right")
 
+#second way to create a label
+my_label["text"] = "New Text"
+
+#another way to create label
+my_label.config(text="new textt")
+
+
+#button
+
 window.mainloop()
