7dffe20600af232fca1991b8c78789a22176dd6e
dhruv-limbasiya <limbasiyadhruv735@gmail.com>
2026-04-10 12:43:35 +0530

    button in tkinter

--- Intermediate/Day 25 (tkinter)/main.py
+++ Intermediate/Day 25 (tkinter)/main.py
@@ -1,4 +1,5 @@
 import tkinter as tkt
+from tkinter import Entry
 
 window = tkt.Tk()
 window.title("GUI program")
@@ -6,7 +7,7 @@ window.minsize(width=500, height=300)
 
 # lable
 my_label = tkt.Label(text="It's label", font=("Arial", 24, "bold"))
-my_label.pack(side="right")
+my_label.pack()
 
 #second way to create a label
 my_label["text"] = "New Text"
@@ -16,5 +17,19 @@ my_label.config(text="new textt")
 
 
 #button
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+button = tkt.Button(text = "Clik me", command=button_clicked)
+button.pack()
+
+
+#Entry
+input = Entry(width = 18)
+input.pack()
+print(input.get())
 
 window.mainloop()
