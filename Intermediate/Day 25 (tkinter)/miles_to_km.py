b0a73f1dc93c7a278f4af72f23182b56aaeac4e9
dhruv-limbasiya <limbasiyadhruv735@gmail.com>
2026-04-10 13:32:29 +0530

    miles to km

--- Intermediate/Day 25 (tkinter)/miles_to_km.py
+++ Intermediate/Day 25 (tkinter)/miles_to_km.py
@@ -3,8 +3,8 @@ from tkinter import *
 
 def miles_to_km():
     miles = float(miles_input.get())
-    km = miles * 1.609
-    # km = round(miles * 1.609)
+    # km = miles * 1.609
+    km = round(miles * 1.609)
     kilometer_result_label.config(text=f"{km}")
 
 window = Tk()
